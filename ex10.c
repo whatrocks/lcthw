bb18b4cc0e139578dff6cea23d2a055fe94e4f4a
Charlie Harrington <charlie@whatrocks.org>
2017-07-29 17:22:36 -0700

    ex10 extra credit

--- ex10.c
+++ ex10.c
@@ -2,51 +2,56 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		printf("ERROR: You need one argument.\n");
+	if (argc < 2) {
+		printf("ERROR: You need at least one argument.\n");
 		// this is how you abort a program
 		return 1;
 	}
 	
-	int i = 0;
-	for (i = 0; argv[1][i] != '\0'; i++) {
-		char letter = argv[1][i];
-		
-		switch(letter) {
-			case 'a':
-			case 'A':
-				printf("%d: 'A'\n", i);
-				break;
-			
-			case 'e':
-			case 'E':
-				printf("%d: 'E'\n", i);
-				break;
-			
-			case 'i':
-			case 'I':
-				printf("%d: 'I'\n", i);
-				break;		
-			
-			case 'o':
-			case 'O':
-				printf("%d: 'O'\n", i);
-				break;
-			
-			case 'u':
-			case 'U':
-				printf("%d: 'U'\n", i);
-				break;
+	for (int j = 1; j < argc; j++) {
 
-			case 'y':
-			case 'Y':
-				printf("%d: 'Y'\n", i);
-				break;
+		for (int i = 0; argv[j][i] != '\0'; i++) {
+			char letter = argv[j][i];
+		
+			switch(letter) {
+				case 'a':
+				case 'A':
+					printf("%d: 'A'\n", i);
+					break;
 			
-			default:
-				printf("%d: %c is not a vowel.\n", i, letter);
+				case 'e':
+				case 'E':
+					printf("%d: 'E'\n", i);
+					break;
+			
+				case 'i':
+				case 'I':
+					printf("%d: 'I'\n", i);
+					break;		
+			
+				case 'o':
+				case 'O':
+					printf("%d: 'O'\n", i);
+					break;
+			
+				case 'u':
+				case 'U':
+					printf("%d: 'U'\n", i);
+					break;
+
+				case 'y':
+				case 'Y':
+					// why i >2? is this a bug?	
+					if (i > 2) {
+						// its only sometimes Y
+						printf("%d: 'Y'\n", i);
+					}
+					break;
+			
+				default:
+					printf("%d: %c is not a vowel.\n", i, letter);
+			}
 		}
-	}
-	
+	}	
 	return 0;
 }
